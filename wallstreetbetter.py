907a9fc35d6d960bcbe59d0a647a85228841a684
rahul-fnu <rahul-fnu@users.noreply.github.com>
2021-02-07 23:21:51 -0500

    Project file completed. Date and post limits can be set mannually by the user

---
+++ wallstreetbetter.py
@@ -0,0 +1,28 @@
+from psaw import PushshiftAPI
+import datetime
+import operator
+def wallstreetbettor():
+    api = PushshiftAPI()
+    start_time = int(datetime.datetime(2021, 2, 3).timestamp())
+    submissions = api.search_submissions(after=start_time, 
+                        subreddit='wallstreetbets',
+                        filter = ['url', 'author', 'title', 'subreddit'], 
+                        limit = 15000)
+    stock_tracker = {}
+    for submission in submissions:
+        words = submission.title.split()
+        cashtag = list(set(filter(lambda word: word.lower().startswith('$'), words)))
+
+        if len(cashtag) > 0:
+            for item in cashtag:
+                tag = item
+                if item[-1] == ',' or item[-1] == ')' or item[-1] == '?':
+                    tag = item[:-1]
+                if tag[1:].isalpha():
+                    if tag in stock_tracker:
+                        stock_tracker[tag] = stock_tracker[tag] + 1
+                    else:
+                        stock_tracker[tag] = 1
+    stock_tracker = sorted(stock_tracker.items(), key=operator.itemgetter(1), reverse=True)
+    return stock_tracker
+print(wallstreetbettor())
